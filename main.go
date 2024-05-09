16c40c5ac8b437ac4235370388f3351cf07e2dfe
Bqstr <beknurnurlan@gmail.com>
2024-05-10 00:36:58 +0500

    one little change

--- main.go
+++ main.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Person struct {
-	ID   int    `json:"id"`
-	NAME string `json:"name"`
-	AGE  int    `json:"age"`
+	ID    int    `json:"id"`
+	NAME  string `json:"name"`
+	MONEY int    `json:"money"`
 }
 
-var list = []Person{Person{0, "person1", 18}, Person{1, "person2", 19}, Person{0, "person1", 18}, Person{2, "person3", 20}}
+var list = []Person{Person{0, "person1", 1111}, Person{1, "person2", 2222}, Person{0, "person1", 2222}, Person{2, "person3", 2222}}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -24,6 +24,9 @@ func main() {
 	router := gin.Default()
 	router.GET("/persons", getPersons)
 	router.POST("/post", addPerson)
+	router.POST("/login", login)
+	router.POST("/addmoney", addMoney)
+
 	router.GET("/hey", getRoute)
 	router.Run(":" + port)
 }
@@ -33,16 +36,81 @@ func getPersons(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, list)
 }
 
+func personExists(id int) bool {
+	for _, p := range list {
+		if p.ID == id {
+			return true
+		}
+	}
+	return false
+}
+
 func addPerson(context *gin.Context) {
+	var requestData struct {
+		ID         int `json:"id"`
+		MoneyToAdd int `json:"money_to_add"`
+	}
+
+	if err := context.BindJSON(&requestData); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
+
+	found := false
+	for i := range list {
+		if list[i].ID == requestData.ID {
+			list[i].MONEY += requestData.MoneyToAdd
+			context.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Added %d money to person with ID %d", requestData.MoneyToAdd, requestData.ID)})
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		context.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Person with ID %d not found", requestData.ID)})
+	}
+}
+
+func addMoney(context *gin.Context) {
+	var requestData struct {
+		ID         int `json:"id"`
+		MoneyToAdd int `json:"money_to_add"`
+	}
+
+	if err := context.BindJSON(&requestData); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON data"})
+		return
+	}
+
+	found := false
+	for i := range list {
+		if list[i].ID == requestData.ID {
+			list[i].MONEY += requestData.MoneyToAdd
+			context.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Added %d money to person with ID %d", requestData.MoneyToAdd, requestData.ID)})
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		context.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("Person with ID %d not found", requestData.ID)})
+	}
+}
+
+func login(context *gin.Context) {
 	var pers Person
 	var err = context.BindJSON(&pers)
 	if err != nil {
 		return
 	}
 
-	list = append(list, pers)
-	fmt.Println(list)
+	if personExists(pers.ID) {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Person with this ID already exists"})
+		return
+	}
+
 	context.IndentedJSON(http.StatusCreated, pers)
+
 }
 
 func getHey(context_gin *gin.Context) {
